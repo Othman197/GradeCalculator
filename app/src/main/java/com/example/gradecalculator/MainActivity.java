79077fc1e40ec0f774460e24c93d7b3efff517fd
Othman197 <101468783+Othman197@users.noreply.github.com>
2022-03-12 21:43:20 +0300

    Initial commit

---
+++ app/src/main/java/com/example/gradecalculator/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.gradecalculator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        final EditText quizeGrade = findViewById(R.id.quizes);
+
+    }
+}
